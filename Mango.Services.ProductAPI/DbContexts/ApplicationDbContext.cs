740fdf96e8212b7ae68a46d4c34d90dd277c8d56
ananthomprakash <ananthomprakash@gmail.com>
2022-06-29 00:41:26 +0530

    Added Development branch and created CURD Operations

---
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "Samosa",
+                Price = 15,
+                Description = "loreum inposium",
+                ImageUrl = "",
+                CategoryName = "Appatizer"
+            });
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ProductId = 2,
+                    Name = "Paneer Tikka",
+                    Price=12,
+                    Description ="loreum imposium 2",
+                    ImageUrl="",
+                    CategoryName="Appatizer"
+                }) ;
+        }
+
+    }
+}
--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,6 +1,22 @@
+using AutoMapper;
+using Mango.Services.ProductAPI;
+using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Adding ProductRepository
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+//setting Mapper
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
